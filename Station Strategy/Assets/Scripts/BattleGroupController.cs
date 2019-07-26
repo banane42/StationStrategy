3f537f879f0015194f32351503df800581fad195
= <=>
2019-07-26 09:08:48 -0500

    Added art assets, Set up battle group scripts

---
+++ Station Strategy/Assets/Scripts/BattleGroupController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGroupController : MonoBehaviour
+{
+
+    TextMesh GroupName;
+    SpriteRenderer Renderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        GroupName = transform.GetChild(0).GetComponent<TextMesh>();
+
+        GroupName.text = "BG\n1";
+        Renderer.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Station Strategy/Assets/Scripts/RenderSorter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderSorter : MonoBehaviour
+{
+
+    public int order;
+    public string layerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.gameObject.GetComponent<MeshRenderer>().sortingOrder = order;
+        this.gameObject.GetComponent<MeshRenderer>().sortingLayerName = layerName;
+
+    }
+}
